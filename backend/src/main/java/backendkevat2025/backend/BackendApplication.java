ce1eb232d55365d6ac6573fe346a65bab1492df4
t0mperi <henri.tomperi@edu.keuda.fi>
2025-02-03 01:07:31 +0200

    s

--- backend/src/main/java/backendkevat2025/backend/BackendApplication.java
+++ backend/src/main/java/backendkevat2025/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package backendkevat2025.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackendApplication {
 
--- backend/src/main/java/com/example/Bookstore/model/Book.java
+++ backend/src/main/java/com/example/Bookstore/model/Book.java
@@ -1,33 +1,34 @@
 package com.example.Bookstore.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String author;
-    private int publicationYear;
-    private String isbn;
-    private double price;
 
     public Book() {}
 
     public Book(String title, String author, int publicationYear, String isbn, double price) {
         this.title = title;
         this.author = author;
-        this.publicationYear = publicationYear;
-        this.isbn = isbn;
-        this.price = price;
     }
 
     // Getters
+    public Long getId() { return id; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
-    public int getPublicationYear() { return publicationYear; }
-    public String getIsbn() { return isbn; }
-    public double getPrice() { return price; }
+    
 
     // Setters
+    public void setId(Long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
     public void setAuthor(String author) { this.author = author; }
-    public void setPublicationYear(int publicationYear) { this.publicationYear = publicationYear; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
-    public void setPrice(double price) { this.price = price; }
+
 }
