d352fd7128607de2b6f92cae294510cccd955d30
t0mperi <henri.tomperi@edu.keuda.fi>
2025-02-03 01:23:12 +0200

    s

--- backend/src/main/java/com/example/Bookstore/web/BookController.java
+++ backend/src/main/java/com/example/Bookstore/web/BookController.java
@@ -29,7 +29,14 @@ public class BookController {
         return "addbook";
     }
     
-    // Save new book
+    // Edit book
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId).orElse(null));
+        return "editbook";
+    }
+    
+    // Save new or edited book
     @PostMapping("/save")
     public String saveBook(Book book) {
         repository.save(book);
